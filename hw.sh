f33fb66d985ee083da2faa82b6db5d4807925c9f
afarooq786 <amirfarooq03@icloud.com>
2022-12-06 15:11:44 -0600

    added

--- hw.sh
+++ hw.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-var people = homework.json;
-var filter = people.filter(age === 25);
-console.log(filter);
+jq (var people = homework.json;
+var filter = people.filter(age === '25');
+console.log(filter);)
